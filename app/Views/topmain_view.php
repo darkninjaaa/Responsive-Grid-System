e215b86dca3e0e214fd61a01a8ec64107c4e7710
darkninjaaa <131391480+darkninjaaa@users.noreply.github.com>
2023-07-13 02:54:20 +0900

    Update topmain_view.php

--- app/Views/topmain_view.php
+++ app/Views/topmain_view.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo HTTP_ROOT.CSS_PATH; ?>/menu.css"; />
 	<link rel="stylesheet" type="text/css" href="<?php echo HTTP_ROOT.CSS_PATH; ?>/grids_24.css"; />
 	<link rel="stylesheet" type="text/css" href="<?php echo HTTP_ROOT.CSS_PATH; ?>/handy-collapse.css"; />
-	<link rel="stylesheet" type="text/css" href="<?php echo HTTP_ROOT.CSS_PATH; ?>/simple-tab.css"; />
+	<link rel="stylesheet" type="text/css" href="<?php echo HTTP_ROOT.CSS_PATH; ?>/simple-tabs.css"; />
 
     <!-- STYLES -->
 
