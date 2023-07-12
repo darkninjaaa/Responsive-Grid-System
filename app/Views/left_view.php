61f4660461a9662ce7059d61df283771eb663b52
darkninjaaa <131391480+darkninjaaa@users.noreply.github.com>
2023-07-13 02:51:48 +0900

    Update left_view.php

--- app/Views/left_view.php
+++ app/Views/left_view.php
@@ -13,31 +13,30 @@
 				<div class="handy-panel left collapsed">
 					<button type="button" class="button is-fullwidth" data-left-button="info">info</button>
 					<div class="handy-panelcontent is-1" data-left-content="info">
-						
-						<div class="tabs_wrapper">
 
+						<div id="tabs_info" class="tabs_wrapper">
 							<div class="tabs_list_outer">
-								<div id="tabs_list_container" class="tabs_list_container">
-									<div class="tabs_pull_right">
-										<span id="tabs_prev"><a href="#">prev</a></span>
-										<span id="tabs_next"><a href="#">next</a></span>
+								<div class="tabs_list_container">
+									<div class="tabs_nav clearfix">
+										<a class="tabs_prev" href="#">prev</a>
+										<a class="tabs_next" href="#">next</a>
 									</div>
-									<div class="tabs_list"><a id="#tabs_seg">segments</a></div>
-									<div class="tabs_list active"><a id="#tabs_path">path</a></div>
-									<div class="tabs_list"><a id="#tabs_doc">doc</a></div>
+									<div class="tabs_list"><a id="#tabs_info_seg">segments</a></div>
+									<div class="tabs_list active"><a id="#tabs_info_path">path</a></div>
+									<div class="tabs_list"><a id="#tabs_info_doc">doc</a></div>
 								</div>
 							</div>
 
-							<div id="tabs_content_container" class="tabs_content_container">
-								<div id="tabs_seg" class="tabs_content">
+							<div class="tabs_content_container">
+								<div id="tabs_info_seg" class="tabs_content">
 									<?php
-										echo 'TotalSegments : '.$TotalSegments.'<br>';
-										echo 'seg_controller : '.$seg_controller.'<br>';
-										echo 'seg_func : '.$seg_func.'<br>';
-										echo 'seg_table : '.$seg_table.'<br>';
+										echo "TotalSegments : ".$TotalSegments."br>";
+										echo "seg_controller : ".$seg_controller."<br>";
+										echo "seg_func : ".$seg_func."<br>";
+										echo "seg_table : ".$seg_table."<br>";
 									?>
 								</div>
-								<div id="tabs_path" class="tabs_content">
+								<div id="tabs_info_path" class="tabs_content">
 									<?php
 										echo "HTTP_HTTPS => ".HTTP_HTTPS."<br>";
 										echo "HTTP_HOST => ".HTTP_HOST."<br>";
@@ -60,11 +59,11 @@
 										echo "APPPATH => ".APPPATH."<br>";
 									?>
 								</div>
-								<div id="tabs_doc" class="tabs_content">
+								<div id="tabs_info_doc" class="tabs_content">
 								</div>
 							</div>
-
 						</div>
+						
 					</div>
 				</div>
 				
