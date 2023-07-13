9727d82cfedefbc0306a7d357b672a5c1b7487e5
darkninjaaa <131391480+darkninjaaa@users.noreply.github.com>
2023-07-13 21:42:21 +0900

    Update right_view.php

--- app/Views/right_view.php
+++ app/Views/right_view.php
@@ -13,5 +13,41 @@
 					</div>
 				</div>
 
+				<div class="handy-panel right collapsed">
+					<button type="button" class="button is-fullwidth" data-right-button="ci">ci</button>
+					<div class="handy-panelcontent is-1" data-right-content="ci">
+
+						<div id="tabs_ci" class="tabs_wrapper">
+							<div class="tabs_list_outer">
+								<div class="tabs_list_container">
+									<div class="tabs_nav clearfix">
+										<a class="tabs_prev" href="#">prev</a>
+										<a class="tabs_next" href="#">next</a>
+									</div>
+									<div class="tabs_list"><a id="#tabs_ci_controller">controller</a></div>
+									<div class="tabs_list active"><a id="#tabs_ci_model">model</a></div>
+									<div class="tabs_list"><a id="#tabs_ci_view">view</a></div>
+								</div>
+							</div>
+
+							<div class="tabs_content_container">
+								<div id="tabs_ci_controller" class="tabs_content">
+									<?php
+										echo "Controller : home"."<br>";
+									?>
+								</div>
+								<div id="tabs_ci_model" class="tabs_content">
+									<?php
+										echo "Model : model"."<br>";
+									?>
+								</div>
+								<div id="tabs_ci_view" class="tabs_content">
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
 			</div>
 		</div>
